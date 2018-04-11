f1976975d0e0cb6ed9a62905c0aff38937ddf3dc
Kaylee Yuhas <kaylee.m.yuhas@gmail.com>
2018-04-11 10:43:03 -0400

    added dictionary and model

--- src/assign5.py
+++ src/assign5.py
@@ -14,29 +14,24 @@ class voice_cmd_vel:
     def __init__(self):
         rospy.on_shutdown(self.cleanup)
 
-		# goals to use throughout program
-		#TODO: replace with actual pose coordinates
-		goal1 = PoseStamped(pose=Pose(1,1,1))
-		goal2 = PoseStamped(pose=Pose(1,1,1))
-		goal3 = PoseStamped(pose=Pose(1,1,1))
-		goal4 = PoseStamped(pose=Pose(1,1,1))
-		self.goals= [goal1, goal2, goal3, goal4]
-		
-		# decoder and stream information
-		modeldir = "../../../model"
-		dict = "./TAR5777/5777.dic"
-		languageModel = "./TAR5777/5777.lm"
+	# goals to use throughout program
+	#TODO: replace with actual pose coordinates
+	goal1 = PoseStamped(pose=Pose(1,1,1))
+	goal2 = PoseStamped(pose=Pose(1,1,1))
+	goal3 = PoseStamped(pose=Pose(1,1,1))
+	goal4 = PoseStamped(pose=Pose(1,1,1))
+	self.goals= [goal1, goal2, goal3, goal4]
 
-		# create publishers
-		self.moveBasePub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
-		self.cmdVelPub = rospy.Publisher('/cmd_vel_mux/input/teleop', Twist, queue_size=10)
+	# create publishers
+	self.moveBasePub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
+	self.cmdVelPub = rospy.Publisher('/cmd_vel_mux/input/teleop', Twist, queue_size=10)
 		
         # subscribe to speech output and amcl pose
         rospy.Subscriber('recognizer/output', String, self.speechCb)
         self.poseSub = rospy.Subscriber('/amcl_pose', PoseWithCovarianceStamped, self.set_pose)
 
-		# robot pose
-		self.robotPose = None
+	# robot pose
+	self.robotPose = None
 
         time.sleep(1) #give publishers time to get set up
         
