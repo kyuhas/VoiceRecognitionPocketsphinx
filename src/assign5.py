c6f5746bf2dfc6f8f76093e51b1709b34c59e5b9
Kaylee Yuhas <kaylee.m.yuhas@gmail.com>
2018-04-11 11:39:56 -0400

    fixed runtime errors

--- src/assign5.py
+++ src/assign5.py
@@ -17,35 +17,33 @@ from std_msgs.msg import Header, String
   #</include>
 
 
-class voice_cmd_vel:
+class voice_cmd_vel():
+	def __init__(self):
+        	rospy.on_shutdown(self.cleanup)
 
-    def __init__(self):
-        rospy.on_shutdown(self.cleanup)
+		# goals to use throughout program
+		#TODO: replace with actual pose coordinates
+		goal1 = PoseStamped(pose=Pose(1,1,1))
+		goal2 = PoseStamped(pose=Pose(1,1,1))
+		goal3 = PoseStamped(pose=Pose(1,1,1))
+		goal4 = PoseStamped(pose=Pose(1,1,1))
+		self.goals= [goal1, goal2, goal3, goal4]
 
-	# goals to use throughout program
-	#TODO: replace with actual pose coordinates
-	goal1 = PoseStamped(pose=Pose(1,1,1))
-	goal2 = PoseStamped(pose=Pose(1,1,1))
-	goal3 = PoseStamped(pose=Pose(1,1,1))
-	goal4 = PoseStamped(pose=Pose(1,1,1))
-	self.goals= [goal1, goal2, goal3, goal4]
-
-	# create publishers
-	self.moveBasePub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
-	self.cmdVelPub = rospy.Publisher('/cmd_vel_mux/input/teleop', Twist, queue_size=10)
+		# create publishers
+		self.moveBasePub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
+		self.cmdVelPub = rospy.Publisher('/cmd_vel_mux/input/teleop', Twist, queue_size=10)
 		
-        # subscribe to speech output and amcl pose
-        rospy.Subscriber('recognizer/output', String, self.speechCb)
-        self.poseSub = rospy.Subscriber('/amcl_pose', PoseWithCovarianceStamped, self.set_pose)
+        	# subscribe to speech output and amcl pose
+        	rospy.Subscriber('recognizer/output', String, self.speechCb)
+        	self.poseSub = rospy.Subscriber('/amcl_pose', PoseWithCovarianceStamped, self.set_pose)
 
-	# robot pose
-	self.robotPose = None
+		# robot pose
+		self.robotPose = None
 
-        time.sleep(1) #give publishers time to get set up
+        	time.sleep(1) #give publishers time to get set up
         
-            
-    def self.set_pose(self, data):
-	self.robotPose = data.pose.pose
+	def set_pose(self, data):
+		self.robotPose = data.pose.pose
 		
 	def waitUntilGoalReached(self, goalPose):
 		tolerance = 0.5
@@ -70,27 +68,27 @@ class voice_cmd_vel:
 		twistMsg.angular.z = angular
 		self.cmdVelPub.publish(twistMsg)
     
-    def speechCb(self, msg):
-	if "stop" in msg.data:
-		print 'hello there!'
-        if msg.data.find("stop") > -1:
-            	# stop the robot
-		self.sendCmdVel(0,0)
-		print 'stop!'
-        elif msg.data.find("turn left") > -1:
-            	# turn robot left
-		self.sendCmdVel(0, -0.2)
-        elif msg.data.find("turn right") > -1:    
-            	# turn robot right
-		self.sendCmdVel(0, 0.2)
-        elif msg.data.find("move forward") > -1:
-            	# move robot forward
-		self.sendCmdVel(0.5, 0)
-        elif msg.data.find("move backward") > -1:    
-            	# move robot backward
-		self.sendCmdVel(-0.5, 0)
-        elif msg.data.find("go to position") > -1:
-            # move to whatever position is designated in the hyp (i.e. one, two, three, etc)
+    	def speechCb(self, msg):
+		if "stop" in msg.data:
+			print 'hello there!'
+        	if msg.data.find("stop") > -1:
+            		# stop the robot
+			self.sendCmdVel(0,0)
+			print 'stop!'
+        	elif msg.data.find("turn left") > -1:
+            		# turn robot left
+			self.sendCmdVel(0, -0.2)
+        	elif msg.data.find("turn right") > -1:    
+            		# turn robot right
+			self.sendCmdVel(0, 0.2)
+        	elif msg.data.find("move forward") > -1:
+            		# move robot forward
+			self.sendCmdVel(0.5, 0)
+        	elif msg.data.find("move backward") > -1:    
+            		# move robot backward
+			self.sendCmdVel(-0.5, 0)
+        	#elif msg.data.find("go to position") > -1:
+            		# move to whatever position is designated in the hyp (i.e. one, two, three, etc)
 			#if msg.data.find("one") > -1:
 			#	self.goToGoalPosition(1)
 			#elif msg.data.find("two") > -1:
@@ -100,8 +98,8 @@ class voice_cmd_vel:
 			#elif msg.data.find("four") > -1:
 			#	self.goToGoalPosition(4)
 
-    def cleanup(self):
-        self.sendCmdVel(0,0)
+    	def cleanup(self):
+        	self.sendCmdVel(0,0)
 
 if __name__=="__main__":
     rospy.init_node('voice_cmd_vel')
