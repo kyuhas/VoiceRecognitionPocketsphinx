064a7060871d3f1c53c74e9f6604396653f9a7a7
Kaylee Yuhas <kaylee.m.yuhas@gmail.com>
2018-04-16 22:40:16 -0400

    soundplay

--- src/assign5.py
+++ src/assign5.py
@@ -9,38 +9,52 @@ from geometry_msgs.msg import Twist
 from move_base_msgs.msg import MoveBaseGoal
 from std_msgs.msg import Header, String
 
+from sound_play.libsoundplay import SoundClient
+
 class voice_cmd_vel():
 	def __init__(self):
 		rospy.on_shutdown(self.cleanup)
 		# create publisher
-		self.cmdVelPub = rospy.Publisher('/cmd_vel_mux/input/teleop', Twist, queue_size=10)
-		
-		# subscribe to speech output and amcl pose
-		rospy.Subscriber('recognizer/output', String, self.speechCb)
+		self.cmdVelPub = rospy.Publisher('/cmd_vel_mux/input/teleop', Twist, queue_size=10)		
+
+		# subscribe to speech output
+		self.sub = rospy.Subscriber('recognizer/output', String, self.speechCb)
 		self.cmd = "stop"
+		self.verifyCmd = "stop"
+		# create soundhandle
+		self.soundhandle = SoundClient()
 
 		time.sleep(1) #give publisher time to get set up
-		#rospy.spin()
 
 	def executeCmd(self, cmdStr):
+		# as of now, the only command it asks verification of is "move backward"
 		if "stop" in cmdStr:
 			# stop the robot
 			self.cmd = "stop"
+			self.soundhandle.say('stop')
 			self.sendCmdVel(0,0)
 		elif "turn left" in cmdStr:
 			# turn robot left
 			self.cmd = "turn left"
+			self.soundhandle.say('turning left')
 			self.sendCmdVel(0, 0.5)
 		elif "turn right" in cmdStr:    
 			# turn robot right
 			self.cmd = "turn right"
+			self.soundhandle.say('turning right')
 			self.sendCmdVel(0, -0.5)
 		elif "move forward" in cmdStr:
 			# move robot forward
 			self.cmd = "move forward"
+			self.soundhandle.say('moving forward')
 			self.sendCmdVel(0.3, 0)
 		elif "move backward" in cmdStr:    
 			# move robot backward
+			# first, stop the robot and have it ask to confirm
+			self.verifyCmd = "move backward"
+			self.soundhandle.say('Are you sure you want me to move backward?')
+		elif "yes" in cmdStr:
+			self.soundhandle.say('Okay, I will now ' + str(self.verifyCmd))
 			self.cmd = "move backward"
 			self.sendCmdVel(-0.3, 0)
 	
@@ -55,8 +69,7 @@ class voice_cmd_vel():
 
     	def cleanup(self):
         	self.sendCmdVel(0,0)
-
-	#TODO: uncomment this out once 
+ 
 	def run(self):
 		# keep calling the same command while the robot is running
 		r = rospy.Rate(10)
